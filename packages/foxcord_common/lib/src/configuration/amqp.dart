baedf27e23be17d2f95fc1de1969f1cbe74fd7e2
Kasefuchs <kasefuchs@protonmail.com>
2023-09-11 23:14:35 +0300

    🚧 Add AMQP configuration model

---
+++ packages/foxcord_common/lib/src/configuration/amqp.dart
@@ -0,0 +1,50 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:dart_amqp/dart_amqp.dart';
+
+part 'amqp.freezed.dart';
+
+part 'amqp.g.dart';
+
+@freezed
+class AMQPConfiguration with _$AMQPConfiguration {
+  /// Default virtual host.
+  static const String defaultVirtualHost = "/";
+
+  /// Default user name.
+  static const String defaultUsername = "guest";
+
+  /// Default password.
+  static const String defaultPassword = "guest";
+
+  const AMQPConfiguration._();
+
+  const factory AMQPConfiguration({
+    /// Host to connect to.
+    required String host,
+
+    /// Port to connect on.
+    required int port,
+
+    /// Virtual host to access during this connection.
+    @Default(AMQPConfiguration.defaultVirtualHost) String virtualHost,
+
+    /// Username to use when authenticating to the server.
+    @Default(AMQPConfiguration.defaultUsername) String username,
+
+    /// Password to use when authenticating to the server.
+    @Default(AMQPConfiguration.defaultPassword) String password,
+  }) = _AMQPConfiguration;
+
+  /// Returns a authenticator based on this configuration.
+  Authenticator get authenticator => PlainAuthenticator(username, password);
+
+  /// Returns AMQP connection settings based on this configuration.
+  ConnectionSettings toConnectionSettings() => ConnectionSettings(
+      host: host,
+      port: port,
+      virtualHost: virtualHost,
+      authProvider: authenticator);
+
+  factory AMQPConfiguration.fromJson(Map<String, dynamic> json) =>
+      _$AMQPConfigurationFromJson(json);
+}
