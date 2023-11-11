b8c11b5e1c03aa59cf3c0542464b9e4d3ae9b13a
Kasefuchs <kasefuchs@protonmail.com>
2023-11-11 19:23:19 +0300

    ⬆️ Migrate to java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
+import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget;
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.springframework.boot.gradle.plugin.SpringBootPlugin
 
 buildscript {
-    extra.apply {
+    rootProject.extra.apply {
         set("springBootVersion", "3.1.5")
         set("kotlinVersion", "1.9.20")
+        set("springDependencyManagementVersion", "1.1.3")
     }
 
     repositories {
@@ -13,20 +16,22 @@ buildscript {
     }
 
     dependencies {
-        val kotlinVersion: String by project.extra
-        val springBootVersion: String by project.extra
+        val kotlinVersion: String by rootProject.extra
+        val springBootVersion: String by rootProject.extra
+        val springDependencyManagementVersion: String by rootProject.extra
 
         kotlin("jvm", kotlinVersion)
         kotlin("plugin.spring", kotlinVersion)
+        kotlin("plugin.allopen", kotlinVersion)
+
         classpath("org.jetbrains.kotlin", "kotlin-gradle-plugin", kotlinVersion)
-        classpath("org.jetbrains.kotlin", "kotlin-allopen", kotlinVersion)
         classpath("org.springframework.boot", "spring-boot-gradle-plugin", springBootVersion)
+        classpath("io.spring.gradle", "dependency-management-plugin", springDependencyManagementVersion)
     }
 }
 
 plugins {
     java
-    id("io.spring.dependency-management") version "1.1.3"
 }
 
 allprojects {
@@ -49,17 +54,23 @@ subprojects {
     }
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    configure<DependencyManagementExtension> {
+        imports {
+            mavenBom(SpringBootPlugin.BOM_COORDINATES) {
+                val kotlinVersion: String by rootProject.extra
+
+                bomProperty("kotlin.version", kotlinVersion)
+            }
+        }
     }
 
     tasks.withType<KotlinJvmCompile> {
         compilerOptions {
-            jvmTarget = JvmTarget.JVM_17
+            jvmTarget = JvmTarget.JVM_21
         }
-    }
-
-    extra.apply {
-        rootProject.extra.properties.forEach { if (!has(it.key)) set(it.key, it.value) }
     }
 }
